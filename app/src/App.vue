e9b7b2c80de80051f22d7953a1967f8968dd2b00
Christian M <chriamue@gmail.com>
2021-12-04 11:12:55 +0100

    :sparkles: added background music

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import Dashboard from "./components/Dashboard.vue";
+import Music from "./components/Music.vue";
 </script>
 
 <template>
   <div id="app">
     <Particles id="tsparticles" url="/particles.json" />
-
+    <Music />
     <Suspense>
       <template #default>
         <Dashboard api="/" />
---
+++ app/src/components/Music.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import christmas from "../assets/christmas.mp3";
+</script>
+<template>
+  <div
+    title="We Wish You A Merry Christmas - Twin Musicom from http://www.twinmusicom.org/ (C) Creative Commons By Attribution License"
+  >
+    <audio
+      controls
+      loop="true"
+      autoplay="true"
+      :src="christmas"
+      preload="auto"
+    ></audio>
+  </div>
+</template>
+
+<style scoped>
+</style>
