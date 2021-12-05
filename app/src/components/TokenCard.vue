6a76f4e57b3a03205b3c726defacf020038ef07d
Christian M <chriamue@gmail.com>
2021-12-05 10:16:15 +0100

    :lipstick: added gift box

--- app/src/components/TokenCard.vue
+++ app/src/components/TokenCard.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import TokenMedia from "./TokenMedia.vue";
 import FileUpload from "./FileUpload.vue";
+import gift_box from "../assets/gift-box.png";
+
 const props = defineProps<{ tokenId: number; url: string; api: string }>();
 const flipUrl = `${props.api}files/${props.tokenId}`;
+
+const uploadEnabled = ref(false);
+const showUpload = () => {
+  uploadEnabled.value = !uploadEnabled.value;
+};
 </script>
 
 <template>
@@ -10,7 +18,10 @@ const flipUrl = `${props.api}files/${props.tokenId}`;
     <div class="card-body">
       <h1>Token: {{ tokenId }}</h1>
       <TokenMedia :url="url" :flipUrl="flipUrl" />
-      <FileUpload :tokenId="tokenId" :api="api" />
+      <div id="uploadbox" title="change gift by uploading new file">
+        <img id="giftbox" @click="showUpload" :src="gift_box" />
+        <FileUpload v-if="uploadEnabled" :tokenId="tokenId" :api="api" />
+      </div>
     </div>
   </div>
 </template>
@@ -18,5 +29,14 @@ const flipUrl = `${props.api}files/${props.tokenId}`;
 <style scoped>
 #tokencard {
   margin: 2em;
+}
+
+#giftbox {
+  float: left;
+  width: 64px;
+}
+
+#uploadbox {
+  display: inline-block;
 }
 </style>
