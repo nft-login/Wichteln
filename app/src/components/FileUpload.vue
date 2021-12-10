da6b162eed23e3f41e365b496b327f1a33ed82a5
Christian M <chriamue@gmail.com>
2021-12-10 10:21:42 +0100

    :lipstick: added upload button

--- app/src/components/FileUpload.vue
+++ app/src/components/FileUpload.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import Loading from "./Loading.vue";
 const props = defineProps<{ tokenId: number; api: string }>();
 const fileInput = ref<any>();
 const file = ref<File>();
-
+const loading = ref(false);
 const handleFileUpload = async () => {
   file.value = fileInput.value.files[0];
   if (file.value) {
+    loading.value = true;
     let data = new FormData();
     data.append("tokenId", props.tokenId.toString());
     data.append("file", file.value, file.value.name);
@@ -14,6 +16,7 @@ const handleFileUpload = async () => {
       method: "post",
       body: data,
     });
+    loading.value = false;
   }
 
   console.log("selected file", file.value);
@@ -21,14 +24,23 @@ const handleFileUpload = async () => {
 </script>
 
 <template>
-  <div class="form-group">
-    <input
-      ref="fileInput"
-      v-on:change="handleFileUpload()"
-      type="file"
-      class="form-control"
-    />
-  </div>
+  <form v-on:submit.prevent>
+    <div class="form-row">
+      <div class="col">
+        <input ref="fileInput" type="file" class="form-control" />
+      </div>
+      <div class="col">
+        <button
+          v-if="!loading"
+          class="btn btn-primary form-control"
+          v-on:click="handleFileUpload()"
+        >
+          wrap gift
+        </button>
+        <Loading v-if="loading" />
+      </div>
+    </div>
+  </form>
 </template>
 
 <style scoped>
