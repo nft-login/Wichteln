91256e7072671339436799131ad0b20de9376b1a
Christian M <chriamue@gmail.com>
2021-12-10 10:54:58 +0100

    :sparkles::card_file_box: save uploaded images as resized jpeg

--- api/src/controllers/filesController.ts
+++ api/src/controllers/filesController.ts
@@ -1,6 +1,7 @@
 import { Post, Get, Route, FormField, UploadedFile, Path, Response, Request } from "tsoa";
-import { readFileSync, rename, existsSync, mkdirSync } from 'fs';
+import { readFileSync, unlinkSync, existsSync, mkdirSync } from 'fs';
 import express from "express";
+import Jimp from 'jimp';
 
 const CONTRACT = process.env.CONTRACT;
 const CHAIN_ID = process.env.CHAIN_ID;
@@ -17,18 +18,18 @@ export class FilesController {
     const contract = request.oidc.user?.contract || CONTRACT;
     const dir = `${file.destination}/${chain_id}/${contract}`;
     !existsSync(dir) && mkdirSync(dir, { recursive: true });
-    rename(file.path, `${dir}/${tokenId}`, (err) => {
-      if (err) {
-        console.error(err);
-      }
-    });
+    Jimp.read(file.path).then(image => {
+      return image.scaleToFit(1280, 1024).quality(80).write(`${dir}/${tokenId}.jpg`);
+    }).finally(() => {
+      unlinkSync(file.path);
+    })
   }
 
   /**
    * @example tokenId "2"
    */
   @Get("{tokenId}")
-  @Response<File, { 'content-type': 'image/png' }>(200)
+  @Response<File, { 'content-type': 'image/jpeg' }>(200)
   public async file(
     @Request() request: express.Request,
     @Path() tokenId: number
@@ -36,6 +37,6 @@ export class FilesController {
     const chain_id = request.oidc.user?.chain_id || CHAIN_ID;
     const contract = request.oidc.user?.contract || CONTRACT;
     const dir = `uploads/${chain_id}/${contract}`;
-    return readFileSync(`${dir}/${tokenId}`);
+    return readFileSync(`${dir}/${tokenId}.jpg`);
   }
 }
